cc75f57103d2d4778979d3a6c695d3184a623542
Asif Al Fattah <asif.alfattah.2@gmail.com>
2024-04-13 17:57:36 +0600

    UI

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    
+    path('courses/', views.courses, name='courses'),
+    path('teachers/', views.teachers, name='teachers'),
+    path('students/', views.students, name='students'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
-from .models import Curriculam
+from .models import *
 
 # Create your views here.
 def index(request):
 
+    
+
+    return render(request, 'index.html')
+
+def courses(request):
     courses = Curriculam.objects.all()
 
-    return render(request, 'index.html', {'courses' : courses})
+    return render(request, 'courses.html', {'courses' : courses})
 
+def teachers(request):
+    faculty = FacultyMember.objects.all()
+    guest = AdjunctFaculty_and_GuestTeacher.objects.all()
+
+    return render(request, 'teachers.html', {'faculty' : faculty, 'guest' : guest})
+
+def students(request):
+    students = StudentInfo_BiSemester.objects.all()
+
+    return render(request, 'students.html', {'students' : students})
