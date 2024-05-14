8b32f955d4cce5e12d424b262cb36f2e6b514f7b
Asif Al Fattah <asif.alfattah.2@gmail.com>
2024-05-14 11:34:39 +0600

    Added Model

--- core/admin.py
+++ core/admin.py
@@ -3,11 +3,15 @@ from .models import *
 
 # Register your models here.
 admin.site.register(Curriculam)
+admin.site.register(Curriculam_Diploma_Intake)
 admin.site.register(FullSyllabus)
 admin.site.register(FacultyMember)
 admin.site.register(AdjunctFaculty_and_GuestTeacher)
 admin.site.register(AllocatedRoom)
 admin.site.register(LabSlot)
-admin.site.register(StudentInfo_BiSemester)
-admin.site.register(StudentInfo_TriSemester)
+admin.site.register(StudentInfo_HSC_Intake)
+admin.site.register(StudentInfo_Diploma_Intake)
 admin.site.register(Semester)
+admin.site.register(ClassSlot)
+admin.site.register(Room_Allocation_to_Diploma_Intake)
+admin.site.register(ClassDaysWithSLots_Diploma_Intake)
--- core/models.py
+++ core/models.py
@@ -31,6 +31,38 @@ class Curriculam(models.Model):
 
     def __str__(self):
         return self.course_code
+
+class StudentInfo_Diploma_Intake(models.Model):
+    year = [
+        ('1st','1st'),
+        ('2nd','2nd'),
+        ('3rd','3rd'),
+        ('4th','4th')
+    ]
+    semester = [
+        ('1st','1st'),
+        ('2nd','2nd')
+    ]
+    
+
+    year = models.CharField(max_length=50, choices=year)
+    semester = models.CharField(max_length=50, choices=semester)
+    number_of_students = models.IntegerField()
+
+    def __str__(self):
+        return self.year+" Year "+self.semester+" Semester "+str(self.number_of_students)+" students"
+
+class Curriculam_Diploma_Intake(models.Model):
+    course_code = models.CharField(max_length=10)
+    course_title = models.CharField(max_length=100)
+    course_credit = models.FloatField()
+    course_class_time_per_week_in_hours = models.FloatField()
+    course_contents = models.TextField()
+
+    semester = models.ForeignKey(StudentInfo_Diploma_Intake, on_delete=models.CASCADE, related_name='diplomacourses')
+
+    def __str__(self):
+        return self.course_code
     
 class FullSyllabus(models.Model):
     syllabus = models.FileField()
@@ -67,7 +99,7 @@ class AllocatedRoom(models.Model):
     max_capacity = models.IntegerField()
 
     def __str__(self):
-        return self.room_no
+        return str(self.room_no)
 
 class LabSlot(models.Model):
     lab_no = models.IntegerField()
@@ -75,7 +107,7 @@ class LabSlot(models.Model):
     def __str__(self):
         return self.lab_no
     
-class StudentInfo_BiSemester(models.Model):
+class StudentInfo_HSC_Intake(models.Model):
     year = [
         ('1st','1st'),
         ('2nd','2nd'),
@@ -100,33 +132,43 @@ class StudentInfo_BiSemester(models.Model):
     number_of_students = models.IntegerField()
 
     def __str__(self):
-        return self.year+" Year "+self.semester+" Semester "+self.section+" Section "
+        return self.year+" Year "+self.semester+" Semester "+self.section+" Section "+str(self.number_of_students)+" students"
 
-class StudentInfo_TriSemester(models.Model):
-    year = [
-        ('1st','1st'),
-        ('2nd','2nd'),
-        ('3rd','3rd'),
-        ('4th','4th')
-    ]
-    semester = [
-        ('1st','1st'),
-        ('2nd','2nd'),
-        ('3rd','3rd')
-    ]
-    section = [
-        ('A','A'),
-        ('B','B'),
-        ('C','C'),
-        ('D','D'),
-        ('E','E'),
-    ]
 
-    year = models.CharField(max_length=50, choices=year)
-    semester = models.CharField(max_length=50, choices=semester)
-    section = models.CharField(max_length=20, choices=section)
-    number_of_students = models.IntegerField()
+    
+class ClassSlot(models.Model):
+
+    starttime = models.TimeField()
+    endtime = models.TimeField()
 
     def __str__(self):
-        return self.year+" Year "+self.semester+" Semester "+self.section+" Section "
+        return f"{str(self.starttime) + ' to ' + str(self.endtime)}"
+
+
+class ClassDaysWithSLots_Diploma_Intake(models.Model):
+
+    days = [
+        ('Monday','Monday'),
+        ('Tuesday','Tuesday'),
+        ('Wednesday','Wednesday'),
+        ('Thursday','Thursday'),
+        ('Friday','Friday'),
+        ('Saturday','Saturday')
+    ]
+
+    day = models.CharField(max_length=50,choices=days)
+    slots = models.ManyToManyField(ClassSlot,related_name='classdayswithslots')
+
+    def __str__(self):
+        return self.day
+
+
+class Room_Allocation_to_Diploma_Intake(models.Model):
+
+    semester = models.ForeignKey(StudentInfo_Diploma_Intake, on_delete=models.CASCADE,  related_name='roomallocations')
+
+    room = models.ForeignKey(AllocatedRoom,on_delete=models.CASCADE, related_name='roomallocations')
+
+    def __str__(self):
+        return f"{self.semester}, Room: {self.room}"
     
